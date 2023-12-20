3a0f6fb3e9d36d63a567c334f74a632249607315
Bill-1991 <altzebill@yahoo.com>
2023-12-20 16:47:13 +0200

    media queries

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,16 +2,18 @@ import React, {useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Col, Row, Card, Button} from "react-bootstrap";
 import "../App.css";
-import vid1 from "../assets/commercevid1.mp4";
+import winsu from "../assets/Winsu.png";
+import ecom from "../assets/e-com.png";
+import stepForm from "../assets/form.png";
 //import { Spring } from "react-spring/renderprops"
 
 
 const Projects = () => {
 
-  const [done, setDone] = useState([{id: "0", name: "E-commerce", vid: vid1, desc: "Responsive e-commerce site for anime", delay: 500, liveSite: "https://splendid-granita-8f1e4f.netlify.app/", source: "https://github.com/Bill-1991/bill-commerce/tree/master", skills: ["Html", "Css", "Bootstrap", "Javascript", "React", "Node", "MySQL", "React-Router", "Anime API"], flip: false},
-                {id: "1", name: "Insurances site", vid: "https://picsum.photos/200/300", desc: "A site for a company called Worthinsurance. Descriptions and packets of every insurance the company offers", delay: 1000, liveSite: "https://worthinsurance.netlify.app", source: "https://github.com/Bill-1991/insurances-site/tree/main/src", skills: ["Html", "Css", "Javscript", "React"], flip: false},
-                {id: "2", name: "Multistep form", vid: "https://picsum.photos/200/300", desc: "Multistep Form. (Not fully constructed in rsponsiveness. A project by Frontmentor!)", delay: 1500, liveSite: "https://warm-sfogliatella-1b0cc2.netlify.app/", source: "https://github.com/Bill-1991/multistep-form-frontmentor/tree/master/src", skills: ["Html", "Css", "Javscript", "React"], flip: false},
-                {id: "3", name: "Tic Tac Toe", vid: "https://picsum.photos/200/300", desc: "Tic Tac Toe minigame. (Not fully constructed. There's no live site on this only the source code. VScom mode only does random moves)", delay: 1500, livesite: "", source: "https://github.com/Bill-1991/Tic-tac-toe", skills: ["Html", "Css", "Javscript"], flip: false}])  
+  const [done, setDone] = useState([{id: "0", name: "E-commerce", img: ecom, desc: "Responsive e-commerce site for anime.(Back-end is down at the moment)", delay: 500, liveSite: "https://splendid-granita-8f1e4f.netlify.app/", source: "https://github.com/Bill-1991/bill-commerce/tree/master", skills: ["Html", "Css", "Bootstrap", "Javascript", "React", "Node", "MySQL", "React-Router", "Anime API"], flip: false},
+                {id: "1", name: "Insurances site", img: winsu, desc: "A site for a company called Worthinsurance. Descriptions and packets of every insurance the company offers", delay: 1000, liveSite: "https://worthinsurance.netlify.app", source: "https://github.com/Bill-1991/insurances-site/tree/main/src", skills: ["Html", "Css", "Javscript", "React"], flip: false},
+                {id: "2", name: "Multistep form", img: stepForm, desc: "Multistep Form. (Not fully constructed in rsponsiveness. A project by frontendmentor.io!)", delay: 1500, liveSite: "https://warm-sfogliatella-1b0cc2.netlify.app/", source: "https://github.com/Bill-1991/multistep-form-frontmentor/tree/master/src", skills: ["Html", "Css", "Javscript", "React"], flip: false},
+                {id: "3", name: "Tic Tac Toe", img: "", desc: "Tic Tac Toe minigame. (Not fully constructed. There's no live site on this only the source code. VScom mode only does random moves)", delay: 1500, livesite: "", source: "https://github.com/Bill-1991/Tic-tac-toe", skills: ["Html", "Css", "Javscript"], flip: false}])  
   
   const handleFlipBack = (id) => {
     let arr = [...done]
@@ -33,10 +35,8 @@ const Projects = () => {
         <Card className={project.flip === false ? "card" : "cardflip"}>
           { project.flip === false ?
             <div className="card-front">
-              <h5 className='title'>{project.name}</h5>
-              <video width="100%" controls>
-                <source src={project.vid} type="video/mp4"/>
-              </video>
+              <p className='title'>{project.name}</p>
+              <img style={{width: "100%"}} src={project.img} alt="No img for this one" />
               <div className="outer-flip">
                 <div className='inner-flip'>
                   <button className="flip" onClick={() => handleFlipBack(project.id)}>Push</button>
@@ -46,24 +46,24 @@ const Projects = () => {
             :
             <div className="card-back">
               <div style={{color: "white"}}>
-                <p>{project.desc}</p>
+                <p className='desc'>{project.desc}</p>
                 <div className="skills" style={{color: "black", width: "100%"}}>
                   <Row style={{width: "90%", position: "relative", left: "5%"}}>
-                    {project.skills.map(skill => <Col key={skill}><p>{skill}</p></Col>)}
+                    {project.skills.map(skill => <Col key={skill}><p className='skill'>{skill}</p></Col>)}
                   </Row>
                 </div>
               </div>
-              <div className='d-grid' style={{display: "flex", gap: "4%", justifyContent: "center"}}>
+              <div className='d-flex' style={{justifyContent: "center", gap: "10%"}}>
                 {
                 project.liveSite ? 
                   <a href={project.liveSite} target="_blank" rel="noreferrer">
-                    <Button className='w-100'>Live</Button>
+                    <Button>Live</Button>
                   </a>
                 :
                   undefined
                 }
                 <a href={project.source} target="_blank" rel="noreferrer">
-                  <Button className="w-100">Source Code</Button>
+                  <Button>Source Code</Button>
                 </a>
               </div>
               <div className='outer-flip'>
